ed29f51ab723313cc15c9bbddad5a6174bc6719d
Lahiru Nuwan <lahirunuwan@Lahirus-MacBook-Air.local>
2026-02-02 09:01:35 +0530

    refactor the code with implementing sources , using CTE grouping and cosmetics cleanups

---
+++ dbt_refactoring_project/models/marts/fct_customer_orders.sql
@@ -0,0 +1,89 @@
+with raw_customers as 
+(
+    select * from {{ source('jaffle_shop', 'customers') }}
+),
+raw_orders as 
+(
+    select * from {{ source('jaffle_shop', 'orders') }}
+),
+raw_payments as 
+(
+    select * from {{ source('stripe', 'payment') }}
+),
+
+p as (
+
+    select ORDERID as order_id, 
+    max(CREATED) as payment_finalized_date, 
+    sum(AMOUNT) / 100.0 as total_amount_paid
+    from raw_payments
+    where STATUS <> 'fail'
+    group by 1   
+),
+
+x as 
+(
+    select
+    p.order_id,
+    sum(t2.total_amount_paid) as clv_bad
+    from paid_orders p
+    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+    group by 1
+    order by p.order_id
+)
+
+
+WITH paid_orders as 
+(
+    select Orders.ID as order_id,
+        Orders.USER_ID    as customer_id,
+        Orders.ORDER_DATE AS order_placed_at,
+            Orders.STATUS AS order_status,
+        p.total_amount_paid,
+        p.payment_finalized_date,
+        C.FIRST_NAME    as customer_first_name,
+            C.LAST_NAME as customer_last_name
+
+    FROM raw_orders as Orders
+    left join  p 
+        ON orders.ID = p.order_id
+    left join raw_customers C 
+    on orders.USER_ID = C.ID
+ ),
+
+customer_orders as 
+(
+    select C.ID as customer_id
+        , min(ORDER_DATE) as first_order_date
+        , max(ORDER_DATE) as most_recent_order_date
+        , count(ORDERS.ID) AS number_of_orders
+
+    from raw_customers C 
+    left join raw_orders as Orders
+    on orders.USER_ID = C.ID 
+    group by 1
+)
+, 
+final as (
+    
+    select
+
+    p.*,
+    ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
+    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
+    CASE 
+        WHEN c.first_order_date = p.order_placed_at
+        THEN 'new'
+        ELSE 'return' 
+    END as nvsr,
+    x.clv_bad as customer_lifetime_value,
+    c.first_order_date as fdos
+
+    FROM paid_orders p
+    left join customer_orders as c USING (customer_id)
+    LEFT OUTER JOIN 
+    x on x.order_id = p.order_id
+    ORDER BY order_id
+)
+select * from final
+
